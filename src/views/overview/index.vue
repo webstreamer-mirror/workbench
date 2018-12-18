de6520cd9c7cef683b0d4f32ab77ec00d328bcb9
Mingyi <mingyi.z@outlook.com>
2018-12-18 21:02:49 +0800

    add vue-markdown component to display md content in page

--- src/views/overview/index.vue
+++ src/views/overview/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="app-container">
-    Overview
+    <vue-markdown>The time of **NOW** is :</vue-markdown><vue-markdown :source="source"></vue-markdown>
+    <vue-markdown :anchor-attributes="anchorAttrs">[click here, start vue-markdown project in new page](https://github.com/miaolz123/vue-markdown)</vue-markdown>
   </div>
 </template>
 <script>
+import VueMarkdown from 'vue-markdown'
 export default {
-  name: 'Overview'
+  name: 'Overview',
+  components: { VueMarkdown },
+  data() {
+    return {
+        source: new Date().toLocaleTimeString(),
+        anchorAttrs: {
+          target: '_blank',
+          rel: 'noopener noreferrer nofollow'
+        }
+      }
+    },
+  mounted () {
+    setInterval(() => {
+      this.source = new Date().toLocaleTimeString();
+    }, 1000);
+  }
 }
 </script>
